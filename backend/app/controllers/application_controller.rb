64a53f9c4ed6be8b151ec6d88fa2852e9c53d506
khargr14 <kay_hargrove@yahoo.com>
2021-04-19 21:30:11 -0400

    update date mock info

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::API
     helpers do
-    def current_user
+     def current_user
         User.first
+     end
+
+
+
+    def logged_in?
     end
   end
 end
