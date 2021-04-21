f626227e41356943825badba4894a4aedae2d748
khargr14 <kay_hargrove@yahoo.com>
2021-04-20 21:34:11 -0400

    fix ReactDom

--- frontend/restarant_entertainment_app/src/index.js
+++ frontend/restarant_entertainment_app/src/index.js
@@ -2,18 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//import * as serviceWorker from './serviceWorker'; 
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+const users = () => []
+const reducer = combineReducers({
+  users
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+ReactDOM.render(<Provider store={ store }><App /></Provider>,document.getElementById('root'));
 
 
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//   <Provider store={ store }>  <App /></Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
